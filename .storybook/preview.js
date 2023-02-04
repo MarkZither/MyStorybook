9d345c059ed91a94e86c33590c975128318617d0
MarkZither <mark.burton@zither-it.co.uk>
2023-02-04 18:43:46 +0100

    learning how it works

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -3,6 +3,16 @@ import { setCompodocJson } from "@storybook/addon-docs/angular";
 import docJson from "../documentation.json";
 setCompodocJson(docJson);
 
+const tokenContext = require.context(
+  '!!raw-loader!../src',
+  true,
+ // /.\.(css|less|scss|svg)$/
+ /.\.(scss)$/
+);
+
+const tokenFiles = tokenContext.keys().map(function (filename) {
+  return { filename: filename, content: tokenContext(filename).default };
+});
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
@@ -11,5 +21,8 @@ export const parameters = {
       color: /(background|color)$/i,
       date: /Date$/,
     },
+  },
+  designToken: {
+    files: tokenFiles
   },
 }
